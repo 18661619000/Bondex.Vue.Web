56ea388394ac2bb4ff9a694245e1dad3db371e47
18661619000 <18661619000>
2020-07-10 09:39:02 +0800

    20200710

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -31,7 +31,7 @@
           <a-form-item>
             <a-input-password
               size="large"
-              placeholder="密码: admin or ant.design 123"
+              placeholder="密码: admin or ant.design 123456"
               v-decorator="[
                 'password',
                 {rules: [{ required: true, message: '请输入密码' }], validateTrigger: 'blur'}
